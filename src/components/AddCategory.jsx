1264420e7196122dcaa2db028887f8487ccdea25
jafh1906 <juanfrancoclasesvirtuales@gmail.com>
2025-01-07 11:43:57 -0500

    Primer commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export const AddCategory = ({ onNewCategory }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+
+  const onSubmit = (e) => {
+    if (inputValue.trim().length <= 1) {
+      return;
+    }
+    onNewCategory(inputValue.trim());
+    setInputValue("");
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Buscar gifs"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+      </form>
+    </>
+  );
+};
